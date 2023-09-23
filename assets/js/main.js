d531da98f98812539535274b7f849e2e12755275
encrypted-cookies <105543893+encrypted-cookies@users.noreply.github.com>
2023-09-23 10:34:53 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@ const sendEmail = (e) =>{
     e.preventDefault()
 
     // serviceID - templateID - #form - publicKey
-    emailjs.sendForm('service_0lqki1s', 'template_fl8nvby', '#contact-form', '3m7ji8BUW2q8mG35w')
+    emailjs.sendForm('service_crujiko', 'template_lmycoiu', '#contact-form', 'jRf4zFNpsTQ4_MqH')
         .then(() =>{
             // Show sent message
             contactMessage.textContent = 'Message sent successfully ✅'
